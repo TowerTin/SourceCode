712be395106af79624eeb6f6724b92aaa469000c
Usagi Ito <usagi@WonderRabbitProject.net>
2013-07-08 12:12:21 +0900

    #1 HitBox.csをsystem/helper.csに移行

---
+++ Ttin/Ttin/system/helper.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.Xna.Framework.Content;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Input;
+using System;
+using System.IO;
+using System.Threading;
+using System.Diagnostics;
+using System.Collections.Generic;
+
+namespace Ttin.system
+{
+
+    /// <summary>
+    /// ヘルパー機能群
+    /// </summary>
+    public class helper
+    {
+        // #1 HitBox.cs から移行
+        // オーバロード化
+        // ToDo: 用途を確認次第さらにリファクタリング
+        public static bool hitcheck(BoundingSphere a, BoundingSphere b)
+        { return a.Intersects(b); }
+        public static bool hitcheck(BoundingSphere a, BoundingBox b)
+        { return a.Intersects(b); }
+    }
+
+}
