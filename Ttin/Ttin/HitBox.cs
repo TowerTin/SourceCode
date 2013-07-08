2d12f2746ffa6370e56eae4b895b59c4ccfa17c0
Usagi Ito <usagi@WonderRabbitProject.net>
2013-07-08 12:01:17 +0900

    #1 namespace に入れた

--- Ttin/Ttin/HitBox.cs
+++ Ttin/Ttin/HitBox.cs
@@ -8,25 +8,29 @@ using System.Threading;
 using System.Diagnostics;
 using System.Collections.Generic;
 
-
-//ユニットと敵の当たり判定
-class HitBox
+namespace Ttin
 {
-    public HitBox()
+
+    //ユニットと敵の当たり判定
+    class HitBox
     {
-        //コンストラクタ
-    }
-    public bool hitcheck(BoundingSphere sphere, BoundingBox box)
-    {
-        //ユニット（キャラリオ専用）と敵の当たり判定
-        //○と□での当たり判定
-        return sphere.Intersects(box);
+        public HitBox()
+        {
+            //コンストラクタ
+        }
+        public bool hitcheck(BoundingSphere sphere, BoundingBox box)
+        {
+            //ユニット（キャラリオ専用）と敵の当たり判定
+            //○と□での当たり判定
+            return sphere.Intersects(box);
+        }
+
+        public bool hitcheck2(BoundingSphere sphere1, BoundingSphere sphere2)
+        {
+            //ユニット（他）と敵の当たり判定
+            //○と○での当たり判定
+            return sphere1.Intersects(sphere2);
+        }
     }
 
-    public bool hitcheck2(BoundingSphere sphere1, BoundingSphere sphere2)
-    {
-        //ユニット（他）と敵の当たり判定
-        //○と○での当たり判定
-        return sphere1.Intersects(sphere2);
-    }
 }
